41d80ae7c7962d4d2ce0d8111926f4395da14963
RossG <rosco20000@gmail.com>
2025-03-31 19:38:04 +0100

    Pausing Test Development Due To Time Constraints.

---
+++ PriorityQueue-main/PriorityQueueTests/UnorderedArrayPriorityQueueTest.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+using PriorityQueue;
+
+namespace PriorityQueueTests
+{
+    public class UnorderedArrayPriorityQueueTest
+    {
+
+        private PriorityQueue<string> queue;
+
+        [SetUp]
+        public void Setup()
+        {
+            queue = new UnorderedArrayPriorityQueue<string>(10);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            queue.Add("A", 3);
+            queue.Add("B", 12);
+            queue.Add("C", 8);
+
+            Assert.AreEqual("High", queue.Head());
+        }
+    }
+}
