d4c2ee8a39d431fc228a986d5defc285deddc372
RossG <rosco20000@gmail.com>
2025-03-31 21:11:09 +0100

    Seems to be in order. UnorderedArrayTests Implemented. All Green!

--- PriorityQueue-main/PriorityQueueTests/UnorderedArrayPriorityQueueTest.cs
+++ PriorityQueue-main/PriorityQueueTests/UnorderedArrayPriorityQueueTest.cs
@@ -15,7 +15,7 @@ namespace PriorityQueueTests
         }
 
         [Test]
-        public void Test1()
+        public void Add_FindHighest()
         {
             queue.Add("A", 3);
             queue.Add("B", 12);
@@ -23,5 +23,51 @@ namespace PriorityQueueTests
 
             Assert.AreEqual("B", queue.Head());
         }
+
+        [Test]
+        public void Remove_RemovesHighestPriorityItem()
+        {
+            queue.Add("A", 2);
+            queue.Add("B", 4);
+            queue.Remove();
+            Assert.AreEqual("A", queue.Head());
+        }
+
+        [Test]
+        public void Add_RemoveAll()
+        {
+            queue.Add("A", 3);
+            queue.Add("B", 12);
+            queue.Add("C", 8);
+            queue.Remove();
+            queue.Remove();
+            queue.Remove();
+            Assert.IsTrue(queue.IsEmpty());
+        }
+
+        [Test]
+        public void CanAdd()
+        {
+            queue.Add("A", 3);
+            Assert.IsFalse(queue.IsEmpty());
+        }
+
+        [Test]
+        public void ExceptionWhenEmpty()
+        {
+            Assert.Throws<QueueUnderflowException>(() => queue.Head());
+        }
+
+        [Test]
+        public void OverflowCheck()
+        {
+            var test = new UnorderedArrayPriorityQueue<String>(2);
+            test.Add("A", 3);
+            test.Add("B", 4);
+            Assert.Throws<QueueOverflowException>(() => test.Add("C", 3));
+
+        }
+
+
     }
 }
